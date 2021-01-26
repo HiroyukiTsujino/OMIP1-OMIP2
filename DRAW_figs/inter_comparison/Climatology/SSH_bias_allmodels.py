[Bug fix] Correct index range for masking Mediterranean. refs #1

DRAW_figs/inter_comparison/Climatology/SSH_bias_allmodels.py
@@ -57,12 +57,12 @@ ncmskcmems.close()
 ################################################
 # Ad hoc modification for Mediterranean (mask out entirely)
 maskcmems[120:140,0:40] = 0
-maskcmems[120:130,355:359] = 0
+maskcmems[120:130,355:360] = 0
 
 maskmed = np.array(np.empty((180,360)),dtype=np.int64)
 maskmed[:,:] = 1
 maskmed[120:140,0:40] = 0
-maskmed[120:130,355:359] = 0
+maskmed[120:130,355:360] = 0
 ################################################
 
 